3a408638e1379a9e658564bf201f0837d06ae42f
Frank Wei <frank@frankwei.xyz>
2021-08-16 17:38:10 +0800

    refactor: useable Modal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,15 +1,27 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Fragment, useRef, useState } from 'react'
+import { Fragment, MutableRefObject, useRef, useState } from 'react'
 import { Dialog, Transition } from '@headlessui/react'
-import { ExclamationIcon } from '@heroicons/react/outline'
+// import { ExclamationIcon } from '@heroicons/react/outline'
+import { ExclamationIcon } from '@heroicons/react/solid'
+
 
 type ModalProps = {
   visible: boolean;
+  title: string;
   setVisiable: (v: boolean) => void;
   onClose?: () => void;
+  icon?: JSX.Element;
+  description?: React.ReactNode;
+  children?: ((cancelButtonRef: MutableRefObject<any>) => React.ReactNode);
 }
 
-export default function Modal({ visible, setVisiable, onClose = () => setVisiable(false) }: ModalProps) {
+export default function Modal({ 
+  visible, setVisiable, onClose = () => setVisiable(false),
+  title,
+  icon: displayedIcon = <ExclamationIcon />,
+  children,
+  description
+}: ModalProps) {
 
   const cancelButtonRef = useRef(null)
 
@@ -51,39 +63,25 @@ export default function Modal({ visible, setVisiable, onClose = () => setVisiabl
             <div className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div className="sm:flex sm:items-start">
-                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                    <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                  <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full sm:mx-0 sm:h-10 sm:w-10">
+                    {/* <ExclamationIcon className="h-6 w-6 text-red-600" aria-hidden="true" /> */}
+                    { displayedIcon }
                   </div>
                   <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
                     <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
-                      Deactivate account
+                      {title}
                     </Dialog.Title>
                     <div className="mt-2">
-                      <p className="text-sm text-gray-500">
-                        Are you sure you want to deactivate your account? All of your data will be permanently removed.
-                        This action cannot be undone.
-                      </p>
+                      { description }
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                <button
-                  type="button"
-                  className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={() => setVisiable(false)}
-                >
-                  Deactivate
-                </button>
-                <button
-                  type="button"
-                  className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={() => setVisiable(false)}
-                  ref={cancelButtonRef}
-                >
-                  Cancel
-                </button>
-              </div>
+              {/* Only render if children is not null (which is by deafult) */}
+              { children && <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                { children(cancelButtonRef) }
+                </div>
+                }
             </div>
           </Transition.Child>
         </div>
