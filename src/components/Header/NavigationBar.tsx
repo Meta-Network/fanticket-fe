f51995547bd55d9dbded6af77a4a73697bcc2a38
Frank Wei <frank@frankwei.xyz>
2021-08-18 16:26:13 +0800

    fix: use `wallet/transfer`

--- src/components/Header/NavigationBar.tsx
+++ src/components/Header/NavigationBar.tsx
@@ -6,7 +6,7 @@ import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
 const navigation = [
     { name: 'Home', href: '/', current: true },
     { name: 'Wallet', href: '/wallet', current: false },
-    { name: 'Send', href: '/token/transfer', current: false },
+    { name: 'Send', href: '/wallet/transfer', current: false },
     { name: 'Swap', href: '/swap', current: false },
     { name: 'My Assets', href: '/my/asset', current: false },
     { name: 'DApps', href: '/dapp', current: false },
